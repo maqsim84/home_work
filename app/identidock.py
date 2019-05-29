87e1a57eb4d7bf55856ecb64e822a80ae9660fa7
maqsim84 <51133152+maqsim84@users.noreply.github.com>
2019-05-29 15:33:01 +0500

    Update identidock.py

--- app/identidock.py
+++ app/identidock.py
@@ -3,14 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   name = default_name
+   name = Hello Word
    header = '<html><head><title>Hello World</title></head><body>'
    body = '''<form method="POST">
              Hello <input type="text" name="name" value="{}">
              <input type="submit" value="submit">
-             </form>
-<img src="/monster/monster.png"/>
-'''.format(name)
+             </form>'''.format(name)
 footer = '</body></html>'
 return header + body + footer
 
