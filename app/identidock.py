d399aaafd093caf07de32c62907accaf24ef165c
maqsim84 <51133152+maqsim84@users.noreply.github.com>
2019-05-29 14:58:54 +0500

    Update identidock.py

--- app/identidock.py
+++ app/identidock.py
@@ -3,21 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-
-<head>
-    <title>Python Flask Hello World App</title>
-    <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet">
-</head>
-<body>
-    <div class="container">
-        <div class="header">
-            <h3 class="text-muted">Hello world</h3>
-        </div>
-        <div class="jumbotron">
-            <h1>Образец задания для компании Megafon</h1>
-            <p class="lead"></p>
-        </div>
+   name = default_name
+   header = '<html><head><title>Hello World</title></head><body>'
+   body = '''<form method="POST">
+             Hello <input type="text" name="name" value="{}">
+             <input type="submit" value="submit">
+             </form>
+<p>You look like a:
+<img src="/monster/monster.png"/>
+'''.format(name)
+footer = '</body></html>'
+return header + body + footer
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
